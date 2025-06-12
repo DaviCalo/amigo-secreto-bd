7ab42b18d7ca141babc0c929906eb7d2dfadc478
Davi C. Pes. <davicalo2d@gmail.com>
2025-06-12 19:59:11 -0300

    first commit

---
+++ PrintPretty.py
@@ -0,0 +1,22 @@
+class PrintPretty:
+    _ANSI_RESET = "\u001B[0m"
+    _ANSI_GREEN = "\u001B[32m"
+    _ANSI_RED = "\u001B[31m"
+    _ANSI_YELLOW = "\u001B[33m"
+
+    def green(self, text: str):
+        print(self._ANSI_GREEN + text + self._ANSI_RESET)
+
+    def yellow(self, text: str):
+        print(self._ANSI_YELLOW + text + self._ANSI_RESET)
+
+    def red(self, text: str):
+        print(self._ANSI_RED + text + self._ANSI_RESET)
+
+    def green(self, text: str):
+        print(self._ANSI_GREEN + text + self._ANSI_RESET)
+
+
+asd = PrintPretty()
+
+asd.green("cuzinho")
